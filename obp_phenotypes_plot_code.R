cc33c41d83815b0676719e87f0f6e533d36e8b0a
snehamokashi <77143783+snehamokashi@users.noreply.github.com>
2022-10-12 13:59:00 -0400

    Update obp_phenotypes_plot_code.R

--- obp_phenotypes_plot_code.R
+++ obp_phenotypes_plot_code.R
@@ -18,10 +18,7 @@ activity_M<-filter(activity, sex=="M")
 # activity plot
 
 act_plot_F<- ggplot(activity_F,aes(Line,mean_value))+geom_boxplot(fill="pink")+theme_classic()+ggtitle("Total activity (females)")+ ylab("Counts")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+xlab("")+scale_y_continuous(limits = c(0, 3000))
-act_plot_F
-
 act_plot_M<- ggplot(activity_M,aes(x=Line,y=mean_value))+geom_boxplot(fill="lightblue")+theme_classic()+ggtitle("Total activity (males)")+ ylab("Counts")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+xlab("")+scale_y_continuous(limits = c(0, 3000))
-act_plot_M
 
 ## heat shock
 
@@ -31,10 +28,8 @@ hs_M<-filter(heat_shock,sex=="M")
 # heat shock plot
 
 hs_plot_F<- ggplot(hs_F,aes(line,Percent_alive))+geom_boxplot(fill="pink")+theme_classic()+ggtitle("Percent alive after heat shock (females)")+ ylab("Percent alive")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0, 100))+xlab("")
-hs_plot_F
 
 hs_plot_M<- ggplot(hs_M,aes(line,Percent_alive))+geom_boxplot(fill="lightblue")+theme_classic()+ggtitle("Percent alive after heat shock (males)")+ ylab("Percent alive")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0, 100))+xlab("")
-hs_plot_M
 
 ## sleep prop day time
 
@@ -45,10 +40,8 @@ slp_d_f<-filter(slp_day,sex=="F")
 # day sleep plot
 
 day_slp_plot_F<- ggplot(slp_d_f,aes(line,mean_sleep_per_ind))+geom_boxplot(fill="pink")+theme_classic()+ggtitle("Proportion of sleep during day (females)")+ ylab("sleep proportion")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+xlab("")+scale_y_continuous(limits = c(0,1.0))
-day_slp_plot_F
 
 day_slp_plot_M<- ggplot(slp_d_m,aes(line,mean_sleep_per_ind))+geom_boxplot(fill="lightblue")+theme_classic()+ggtitle("Proportion of sleep during day (males)")+ ylab("sleep proportion")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+xlab("")+scale_y_continuous(limits = c(0,1.0))
-day_slp_plot_M
 
 ## night sleep
 
@@ -59,23 +52,8 @@ slp_n_f<-filter(slp_night,sex=="F")
 # night sleep plot
 
 night_slp_plot_F<- ggplot(slp_n_f,aes(line,mean_sleep_per_ind))+geom_boxplot(fill="pink")+theme_classic()+ggtitle("Proportion of sleep during night (females)")+ ylab("sleep proportion")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+xlab("")+scale_y_continuous(limits = c(0,1.0))
-night_slp_plot_F
 
 night_slp_plot_M<- ggplot(slp_n_m,aes(line,mean_sleep_per_ind))+geom_boxplot(fill="lightblue")+theme_classic()+ggtitle("Proportion of sleep during night (males)")+ ylab("sleep proportion")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+xlab("")+scale_y_continuous(limits = c(0,1.0))
-night_slp_plot_M
-
-## survival- lifespan
-
-surv_lifespan_m<-filter(surv,sex=="M")
-surv_lifespan_f<-filter(surv,sex=="F")
-
-# lifespan under starvation plot
-
-lifespan_plot_F<- ggplot(surv_lifespan_f,aes(Line,Lifespan_under_starvation_hours))+geom_boxplot(fill="pink")+theme_classic()+ggtitle("Lifespan under starvation (females)")+ ylab("Lifespan (h)")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0, 80))+xlab("")
-lifespan_plot_F
-
-lifespan_plot_M<- ggplot(surv_lifespan_m,aes(Line,Lifespan_under_starvation_hours))+geom_boxplot(fill="lightblue")+theme_classic()+ggtitle("Lifespan under starvation (males)")+ ylab("Lifespan (h)")+theme(aspect.ratio=1)+theme(plot.title = element_text(hjust = 0.5))+scale_y_continuous(limits = c(0, 80))+xlab("")
-lifespan_plot_M
 
 # survival curves
 
@@ -86,18 +64,14 @@ library(survminer)
 
 fit_f <- survfit(Surv(Lifespan_under_starvation_hours) ~ Line, data = surv_lifespan_f)
 surv_curv_f<-ggsurvplot(fit_f,palette = c("black","red"))+ggtitle("Survival curves under starvation (females)")
-surv_curv_f
-
 
 fit_m <- survfit(Surv(Lifespan_under_starvation_hours) ~ Line, data = surv_lifespan_m)
 surv_curv_m<-ggsurvplot(fit_m,palette = c("black","red"))+ggtitle("Survival curves under starvation (males)")
-surv_curv_m
 
 ### putting all plots together
 
 fig_1<-plot_grid(surv_curv_m,surv_curv_f,lifespan_plot_M,lifespan_plot_F,hs_plot_M,hs_plot_F,labels="AUTO", ncol=2,align="h")
 fig_1
-
 
 fig_2<-plot_grid(act_plot_M,act_plot_F,day_slp_plot_M,day_slp_plot_F,night_slp_plot_M,night_slp_plot_F, labels = "AUTO", ncol=2, align="h")
 fig_2
